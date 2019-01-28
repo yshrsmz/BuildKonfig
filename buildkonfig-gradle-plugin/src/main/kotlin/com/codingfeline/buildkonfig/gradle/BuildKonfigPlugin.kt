811687652c6e1998cdf7f46fa861d1445beaa1f2
Yasuhiro Shimizu <the.phantom.bane@gmail.com>
2019-01-28 22:41:55 +0900

    check if Kotlin Multiplaform Plugin is applied

--- buildkonfig-gradle-plugin/src/main/kotlin/com/codingfeline/buildkonfig/gradle/BuildKonfigPlugin.kt
+++ buildkonfig-gradle-plugin/src/main/kotlin/com/codingfeline/buildkonfig/gradle/BuildKonfigPlugin.kt
@@ -4,12 +4,30 @@ package com.codingfeline.buildkonfig.gradle
 import org.gradle.api.Plugin
 import org.gradle.api.Project
 import org.gradle.api.logging.Logging
+import org.jetbrains.kotlin.gradle.plugin.KotlinMultiplatformPluginWrapper
 import java.io.File
 
 open class BuildKonfigPlugin : Plugin<Project> {
 
 
     override fun apply(target: Project) {
+
+        var isMultiplatform = false
+        target.plugins.all { p ->
+            if (p is KotlinMultiplatformPluginWrapper) {
+                isMultiplatform = true
+            }
+        }
+
+        println("isMultiplatform: $isMultiplatform")
+
+        if (!isMultiplatform) {
+            throw IllegalStateException(
+                "BuildKonfig Gradle plugin applied in project '${target.path}' " +
+                        "but no supported Kotlin plugin was found"
+            )
+        }
+
         val objectFactory = target.objects
 
         val outputDirectory = File(target.buildDir, "buildKonfig")
@@ -18,12 +36,11 @@ open class BuildKonfigPlugin : Plugin<Project> {
 
         val logger = Logging.getLogger(BuildKonfigPlugin::class.java)
 
-//        val defaultConfig = instantiator.newInstance(PlatformConfigDsl, "defaults")
-//        println(defaultConfig)
-
         extension.defaultConfigs = objectFactory.newInstance(PlatformConfigDsl::class.java, "defaults", logger)
-        extension.targetConfigs =
-            target.container(PlatformConfigDsl::class.java, PlatformConfigFactory(objectFactory, logger))
+        extension.targetConfigs = target.container(
+            PlatformConfigDsl::class.java,
+            PlatformConfigFactory(objectFactory, logger)
+        )
 
         val task = target.tasks.create("generateBuildKonfig", BuildKonfigTask::class.java) {
             it.setExtension(extension)
@@ -31,5 +48,9 @@ open class BuildKonfigPlugin : Plugin<Project> {
             it.group = "buildKonfig"
             it.description = "Generate BuildKonfig"
         } as BuildKonfigTask
+    }
+
+    fun configure(project: Project) {
+
     }
 }
