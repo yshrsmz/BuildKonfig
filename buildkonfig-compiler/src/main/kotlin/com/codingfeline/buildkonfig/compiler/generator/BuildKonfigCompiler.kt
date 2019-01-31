5b6dbf73074f1a6c373873cb896077bd77fdc428
Yasuhiro Shimizu <the.phantom.bane@gmail.com>
2019-01-31 19:58:19 +0900

    :construction:

--- buildkonfig-compiler/src/main/kotlin/com/codingfeline/buildkonfig/compiler/generator/BuildKonfigCompiler.kt
+++ buildkonfig-compiler/src/main/kotlin/com/codingfeline/buildkonfig/compiler/generator/BuildKonfigCompiler.kt
@@ -1,4 +1,26 @@
-package com.codingfeline.buildkonfig.compiler
+package com.codingfeline.buildkonfig.compiler.generator
+
+import com.codingfeline.buildkonfig.compiler.Logger
+import com.codingfeline.buildkonfig.compiler.TargetConfigFile
+import java.io.OutputStreamWriter
 
 object BuildKonfigCompiler {
+
+    fun compileCommon(
+        packageName: String,
+        configFile: TargetConfigFile,
+        writer: (String) -> OutputStreamWriter,
+        logger: Logger
+    ) {
+
+    }
+
+    fun compileTarget(
+        packageName: String,
+        configFile: TargetConfigFile,
+        writer: (String) -> OutputStreamWriter,
+        logger: Logger
+    ) {
+
+    }
 }
