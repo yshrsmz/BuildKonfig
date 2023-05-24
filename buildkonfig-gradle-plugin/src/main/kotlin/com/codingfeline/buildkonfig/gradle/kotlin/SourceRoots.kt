cece0af84179fb98a02b252c8720059cca1a62e5
jason <jason.sparc.7@gmail.com>
2023-05-25 01:00:03 +0800

    Added single-target counterpart

--- buildkonfig-gradle-plugin/src/main/kotlin/com/codingfeline/buildkonfig/gradle/kotlin/SourceRoots.kt
+++ buildkonfig-gradle-plugin/src/main/kotlin/com/codingfeline/buildkonfig/gradle/kotlin/SourceRoots.kt
@@ -1,8 +1,10 @@
 package com.codingfeline.buildkonfig.gradle.kotlin
 
 import org.jetbrains.kotlin.gradle.dsl.KotlinMultiplatformExtension
+import org.jetbrains.kotlin.gradle.dsl.KotlinSingleTargetExtension
 import org.jetbrains.kotlin.gradle.plugin.KotlinPlatformType
 import org.jetbrains.kotlin.gradle.plugin.KotlinSourceSet
+import org.jetbrains.kotlin.gradle.plugin.KotlinTarget
 import org.jetbrains.kotlin.gradle.plugin.mpp.KotlinNativeTarget
 
 data class Source(
@@ -13,38 +15,45 @@ data class Source(
     val sourceSets: List<KotlinSourceSet>
 )
 
-internal fun KotlinMultiplatformExtension.sources(): List<Source> {
-    return targets
-        .flatMap { target ->
-            target.compilations.filter { !it.name.endsWith(suffix = "Test", ignoreCase = true) }
-                .map { compilation ->
-                    val (defaultSourceSet, sourceSets) = when (target.platformType) {
-                        KotlinPlatformType.androidJvm -> {
-                            val default = compilation.allKotlinSourceSets
-                                .first { it.name == "${target.name}Main" }
-
-                            val all = compilation.allKotlinSourceSets
-                                .filter {
-                                    it.name != "${target.name}Main" &&
-                                            it.name != compilation.defaultSourceSet.name
-                                }
-
-                            default to all
-                        }
-                        else -> {
-                            compilation.defaultSourceSet to compilation.allKotlinSourceSets
-                                .filter { it.name != compilation.defaultSourceSet.name }
-                        }
-                    }
-                    Source(
-                        type = compilation.platformType,
-                        nativePresetName = ((target as? KotlinNativeTarget)?.preset?.name),
-                        name = defaultSourceSet.name,
-                        defaultSourceSet = defaultSourceSet,
-                        sourceSets = sourceSets
-                    )
-                }
-        }
+internal fun KotlinSingleTargetExtension<out KotlinTarget>.sources(): List<Source> {
+    return sourcesForTarget(target)
         .distinct()
         .sortedBy { it.sourceSets.size }
 }
+
+internal fun KotlinMultiplatformExtension.sources(): List<Source> {
+    return targets
+        .flatMap { sourcesForTarget(it) }
+        .distinct()
+        .sortedBy { it.sourceSets.size }
+}
+
+private fun sourcesForTarget(target: KotlinTarget) = target.compilations
+    .filter { !it.name.endsWith(suffix = "Test", ignoreCase = true) }
+    .map { compilation ->
+        val (defaultSourceSet, sourceSets) = when (target.platformType) {
+            KotlinPlatformType.androidJvm -> {
+                val default = compilation.allKotlinSourceSets
+                    .first { it.name == "${target.name}Main" }
+
+                val all = compilation.allKotlinSourceSets
+                    .filter {
+                        it.name != "${target.name}Main" &&
+                                it.name != compilation.defaultSourceSet.name
+                    }
+
+                default to all
+            }
+            else -> {
+                compilation.defaultSourceSet to compilation.allKotlinSourceSets
+                    .filter { it.name != compilation.defaultSourceSet.name }
+            }
+        }
+        Source(
+            type = compilation.platformType,
+            nativePresetName = ((target as? KotlinNativeTarget)?.preset?.name),
+            name = defaultSourceSet.name,
+            defaultSourceSet = defaultSourceSet,
+            sourceSets = sourceSets
+        )
+    }
