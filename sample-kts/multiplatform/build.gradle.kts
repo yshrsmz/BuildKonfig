a9131602ad845034c9554c3ce721c7d8c38c45dc
jason <jason.sparc.7@gmail.com>
2023-05-31 08:19:32 +0800

    JVM target needs to be the same to build android samples

--- sample-kts/multiplatform/build.gradle.kts
+++ sample-kts/multiplatform/build.gradle.kts
@@ -12,10 +12,19 @@ plugins {
 android {
     namespace = "com.example.namespace"
     compileSdk = 28
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
 }
 
 kotlin {
-    android()
+    android {
+        compilations.all {
+            kotlinOptions.jvmTarget = "1.8"
+        }
+    }
     jvm()
     js(IR) {
         browser()
