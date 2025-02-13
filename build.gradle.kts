47b9c3129cbef543e633c76cad9d7aa7ec1cb4e9
RyuNen344 <s1100633@outlook.com>
2025-02-13 19:46:49 +0900

    refactor: replace deprecated apis

--- build.gradle.kts
+++ build.gradle.kts
@@ -28,7 +28,7 @@ allprojects {
 }
 
 tasks.register("clean", Delete::class.java) {
-    delete(rootProject.buildDir)
+    delete(rootProject.layout.buildDirectory)
 }
 
 tasks.wrapper {
@@ -38,7 +38,7 @@ tasks.wrapper {
 
 // https://github.com/ben-manes/gradle-versions-plugin
 fun isNonStable(version: String): Boolean {
-    val stableKeyword = listOf("RELEASE", "FINAL", "GA").any { version.toUpperCase().contains(it) }
+    val stableKeyword = listOf("RELEASE", "FINAL", "GA").any { version.uppercase().contains(it) }
     val regex = "^[0-9,.v-]+(-r)?$".toRegex()
     val isStable = stableKeyword || regex.matches(version)
     return isStable.not()
